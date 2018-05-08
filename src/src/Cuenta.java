773dc3cfd86a9a2b0b018cc13d370f5623c6088e
Antonio Maña Gomez <amg@lcc.uma.es>
2018-05-08 11:54:36 +0200

    ok

---
+++ src/src/Cuenta.java
@@ -0,0 +1,29 @@
+package src;
+
+// Esta es la versión depurada mediante tests JUnit
+import test.IngresoNegativoException;
+
+public class Cuenta {
+    private double saldo;
+
+    public Cuenta(double si) {
+        saldo = Math.max(0, si);
+    }
+
+    public void ingresa(double cantidad) {
+        if (cantidad < 0) {
+            throw new test.IngresoNegativoException("ingreso negativo: " + cantidad);
+        }
+        saldo = saldo + cantidad;
+    }
+
+    public double extrae(double cantidad) {
+        final double extraido = Math.min(saldo, cantidad);
+        saldo -= extraido;
+        return extraido;
+    }
+
+    public double saldo() {
+        return saldo;
+    }
+}
